2855b11a609b8bd0f48fe0c7b5371ad4da99bd88
smabari <mdazizulbarishafin@gmail.com  git config --global user.email mdazizulbarishafin@gmail.com>
2025-05-02 20:24:49 +0600

    kept the data in localstorage

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLoaderData, useParams } from "react-router";
+import { addToStoredDB } from "../../utility/addToDB";
 
 const BookDetails = () => {
   const { id } = useParams();
@@ -10,12 +11,22 @@ const BookDetails = () => {
     singleBook;
   //   console.log(singleBook);
 
+  const handleMarkAsRead = (id) => {
+    // store with id
+    // where to store
+    // store as a array or collection
+    // don't add if already added
+    addToStoredDB(id);
+  };
+
   return (
     <div className="w-2/3 mx-auto">
       <img src={image} className="w-48" alt="" />
       <h4>{bookName}</h4>
       <div className="flex gap-3">
-        <button className="btn btn-accent">Mark as Read</button>
+        <button onClick={() => handleMarkAsRead(id)} className="btn btn-accent">
+          Mark as Read
+        </button>
         <button className="btn btn-primary">Add to Wishlist</button>
       </div>
     </div>
