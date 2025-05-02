be669f1c342af2047ee51e964a706ad8cc7b9ad0
smabari <mdazizulbarishafin@gmail.com  git config --global user.email mdazizulbarishafin@gmail.com>
2025-05-02 13:08:16 +0600

    dynamyc book links setup

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,5 +1,6 @@
-import React, { use } from "react";
+// import React, { use } from "react";
 import { IoIosStarHalf } from "react-icons/io";
+import { Link } from "react-router";
 
 const Book = ({ singleBook }) => {
   //   const data = use(bookPromise);
@@ -8,42 +9,43 @@ const Book = ({ singleBook }) => {
   console.log(singleBook);
   const {
     bookName,
+    bookId,
     author,
     image,
     rating,
     category,
-    publisher,
     yearOfPublishing,
     tags,
-    review,
   } = singleBook;
 
   return (
-    <div className="card bg-base-100 w-96 shadow-sm border p-6">
-      <figure className="p-4 bg-gray-100 w-2/3 mx-auto">
-        <img className="h-[166px]" src={image} alt="Shoes" />
-      </figure>
-      <div className="card-body">
-        <div className="flex justify-center gap-10">
-          {tags.map((tag) => (
-            <button className="">{tag}</button>
-          ))}
-        </div>
-        <h2 className="card-title">
-          {bookName} 
-          <div className="badge badge-secondary">{yearOfPublishing}</div>
-        </h2>
-        <p>Book by: {publisher}</p>
-        <div className="border border-dashed"></div>
-        <div className="card-actions justify-end">
-          <div className="badge badge-outline">{category}</div>
-          <div className="badge badge-outline">
-            {rating}
-            <IoIosStarHalf />
+    <Link to={`/bookDetails/${bookId}`}>
+      <div className="card bg-base-100 w-96 shadow-lg border p-6">
+        <figure className="p-4 bg-gray-100 w-2/3 mx-auto">
+          <img className="h-[166px]" src={image} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <div className="flex justify-center gap-10">
+            {tags.map((tag) => (
+              <button className="">{tag}</button>
+            ))}
+          </div>
+          <h2 className="card-title">
+            {bookName}
+            <div className="badge badge-secondary">{yearOfPublishing}</div>
+          </h2>
+          <p>Book by: {author}</p>
+          <div className="border border-dashed"></div>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline">{category}</div>
+            <div className="badge badge-outline">
+              {rating}
+              <IoIosStarHalf />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
---
+++ src/pages/BookDetails/BookDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BookDetails = () => {
+    return (
+        <div>
+            <h4>Book Details</h4>
+        </div>
+    );
+};
+
+export default BookDetails;
